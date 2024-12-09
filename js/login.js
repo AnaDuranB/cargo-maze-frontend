f8d5b18e14339571927fd5c0cec280761a138d89
MiltonGutierrez <milton.gutierrez-l@mail.escuelaing.edu.co>
2024-12-09 05:47:06 -0500

    FUNCIONA

--- js/login.js
+++ js/login.js
@@ -3,34 +3,45 @@ const login = (() => {
 
     const loginWithMicrosoft = async () => {
         try {
-            // Redirect to Azure authentication
-            //window.location.href = "https://proyectoarsw.duckdns.org/login/oauth2/authorization/aad";
-            window.location.href = "http://localhost:8080/oauth2/authorization/aad";
+            loginWithMicrosoftInNewTab();
         } catch (error) {
             console.error("Error during authentication: ", error);
         }
     };
 
+    const loginWithMicrosoftInNewTab = async () => {
+        const popup = window.open(
+            "http://localhost:8080/oauth2/authorization/aad",
+            "_blank",
+            "width=600,height=600"
+        );
+
+        window.addEventListener("message", (event) => {
+            if (event.origin !== "http://localhost:4200") return; // Validar origen del mensaje
+            if (event.data.status === "success") {
+                console.log("Autenticación completada."); 
+                sessionStorage.setItem("nickname", getCookie('display_name'));
+                console.log(sessionStorage.getItem("nickname"));
+                const authToken = getCookie("auth_token");    
+                console.log('authToken:', authToken); 
+                initializeUserSession();
+
+            }
+        });
+    };
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+        return null;
+    }
+
+
     const initializeUserSession = async () => {
         try {
-            const queryParams = new URLSearchParams(window.location.search);
 
-            const token = queryParams.get("token");
-            const displayName = queryParams.get("displayName");
-
-            console.log("Token:", token);
-            console.log("DisplayName:", displayName);
-
-            if (token == null || displayName == null) {
-                throw new Error("Token o DisplayName no están presentes en la URL.");
-            }
-            await handleNickname(displayName);
-
-            sessionStorage.setItem("token", token);
-
-            console.log(sessionStorage.getItem("nickname"));
-            console.log(sessionStorage.getItem("token"));
-
+            await handleNickname(sessionStorage.getItem("nickname"));
             window.location.href = "./sessionMenu.html";
 
         } catch (error) {
@@ -39,15 +50,9 @@ const login = (() => {
     };
 
     const handleNickname = async (newNickname) => {
-        sessionStorage.clear();
         try {
             const player = await api.verifyNickname(newNickname);
-            if (player) {
-                sessionStorage.setItem("nickname", newNickname);
-            } else {
-                await api.login(newNickname);
-                sessionStorage.setItem("nickname", newNickname);
-            }
+            console.log('player:', player);
         } catch (error) {
             console.error("Error al manejar el nickname:", error);
             throw error;
@@ -57,9 +62,5 @@ const login = (() => {
     return {
         loginWithMicrosoft,
         getDisplayName: () => sessionStorage.getItem("nickname"),
-        init: function () {
-            initializeUserSession();
-        }
     };
 })();
-login.init();
